74a13a2cdd3865b82146015383e190a660cf6581
lucaslomba <lucasazevedolomba@gmail.com>
2023-05-08 16:27:13 -0300

    Criando modal de novas transacoes

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,36 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from "./styles";
+import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
+
+export function NewTransactionModal() {
+    return (
+        <Dialog.Portal>
+            <Overlay />
+            <Content>
+                <Dialog.Title>Nova transação</Dialog.Title>
+                <CloseButton>
+                    <X size={24}/>
+                </CloseButton>
+
+                <form action="">
+                    <input type="text" placeholder="Descrição" required/>
+                    <input type="number" placeholder="Preço" required/>
+                    <input type="text" placeholder="Categoria" required/>
+
+                    <TransactionType>
+                        <TransactionTypeButton variant="income" value="income">
+                            <ArrowCircleUp />
+                            Entrada
+                        </TransactionTypeButton>
+                        <TransactionTypeButton variant="outcome" value="outcome">
+                            <ArrowCircleDown />
+                            Saída
+                        </TransactionTypeButton>
+                    </TransactionType>
+
+                    <button type="submit">Cadastrar</button>
+                </form>
+            </Content>
+        </Dialog.Portal>
+    )
+}
